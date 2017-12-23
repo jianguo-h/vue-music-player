873d5bdb1c6b3c8d5b13a679a6690ad974a60c88
jianguo-h <1060684755@qq.com>
2017-12-24 00:22:19 +0800

    规范 api Promise写法

--- src/api/index.js
+++ src/api/index.js
@@ -5,43 +5,55 @@ import request from './request';
 export default {
     // 根据关键字搜索
     search(keyword) {
-        const requestData = {
-            keyword
-        };
-        console.log(">>> [api.requestData] 根据关键字搜索", requestData);
-        return axios.post(request.search, requestData);
+        return new Promise((resolve, reject) => {
+            const params = {
+                keyword
+            };
+            console.log(">>> [api.params] 根据关键字搜索", params);
+            axios.post(request.search, params).then(res => {
+                resolve(res);
+            }).catch(reject);
+        });
     },
     // 获取静态json数据中的歌曲列表
     getList(path) {
-        console.log('>>> [api.requestData] 获取静态json数据中的歌曲列表', path);
-        return axios.post(request[path]);
+        return new Promise((resolve, reject) => {
+            console.log('>>> [api.params] 获取静态json数据中的歌曲列表', path);
+            axios.post(request[path]).then(res => {
+                resolve(res);
+            }).catch(reject);
+        });
     },
     // 获取歌曲的一些信息
     getSongInfo(songName, page = 1) {
-        const requestData = {
-            page,
-            pagesize: 20,
-            keyword: songName,
-            platform: "WebFilter",
-            userid: -1,
-            iscorrection: 1,
-            privilege_filter: 0,
-            filter: 2
-        };
-        console.log('>>> [api.requestData] 获取歌曲的一些信息', requestData);
-        return axios.get(request.songsearch, {
-            params: { ...requestData }
+        return new Promise((resolve, reject) => {
+            const params = {
+                page,
+                pagesize: 20,
+                keyword: songName,
+                platform: "WebFilter",
+                userid: -1,
+                iscorrection: 1,
+                privilege_filter: 0,
+                filter: 2
+            };
+            console.log('>>> [api.params] 获取歌曲的一些信息', params);
+            axios.get(request.songsearch, { params }).then(res => {
+                resolve(res);
+            }).catch(reject);
         });
     },
     // 根据hash值获取歌曲的信息
     play(hash) {
-        const requestData = {
-            r: "play/getdata",
-            hash
-        };
-        console.log('>>> [api.requestData] 根据hash值获取歌曲的信息', requestData);
-        return axios.get(request.play, {
-            params: { ...requestData }
+        return new Promise((resolve, reject) => {
+            const params = {
+                r: "play/getdata",
+                hash
+            };
+            console.log('>>> [api.params] 根据hash值获取歌曲的信息', params);
+            axios.get(request.play, { params }).then(res => {
+                resolve(res);
+            }).catch(reject);
         });
     }
 }
