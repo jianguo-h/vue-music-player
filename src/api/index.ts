bfc4198d38105cd2aceae4e2c5fdae40ee5f3c71
jianguo-h <1060684755@qq.com>
2019-10-12 10:14:02 +0800

    feat: 部分js文件迁移至ts

---
+++ src/api/index.ts
@@ -0,0 +1,72 @@
+// 统一管理各个请求
+import axios from 'axios';
+import request from './request';
+
+export default {
+  // 根据关键字搜索
+  search(keyword: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const params = {
+        keyword
+      };
+      console.log('>>> [api.params] 根据关键字搜索', params);
+      axios
+        .post(request.search, params)
+        .then(res => {
+          resolve(res);
+        })
+        .catch(reject);
+    });
+  },
+  // 获取静态json数据中的歌曲列表
+  getList(path: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      console.log('>>> [api.params] 获取静态json数据中的歌曲列表', path);
+      axios
+        .post(request[path])
+        .then(res => {
+          resolve(res);
+        })
+        .catch(reject);
+    });
+  },
+  // 获取歌曲的一些信息
+  getSongInfo(songName: string, page: number = 1): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const params = {
+        page,
+        pagesize: 20,
+        keyword: songName,
+        platform: 'WebFilter',
+        userid: -1,
+        iscorrection: 1,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        privilege_filter: 0,
+        filter: 2
+      };
+      console.log('>>> [api.params] 获取歌曲的一些信息', params);
+      axios
+        .get(request.songsearch, { params })
+        .then(res => {
+          resolve(res);
+        })
+        .catch(reject);
+    });
+  },
+  // 根据hash值获取歌曲的信息
+  play(hash: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const params = {
+        r: 'play/getdata',
+        hash
+      };
+      console.log('>>> [api.params] 根据hash值获取歌曲的信息', params);
+      axios
+        .get(request.play, { params })
+        .then(res => {
+          resolve(res);
+        })
+        .catch(reject);
+    });
+  }
+};
---
+++ src/js/adapt.ts
@@ -0,0 +1,53 @@
+// rem 适配手机屏幕
+export default function() {
+  // const evt = "onorientationchange" in window ? "onorientationchange" : "resize";
+  const isIPhone = window.navigator.appVersion.match(/iphone/gi);
+  let dpr = window.devicePixelRatio;
+  if (isIPhone) {
+    if (dpr >= 3 && (!dpr || dpr >= 3)) {
+      dpr = 3;
+    } else if (dpr >= 2 && (!dpr || dpr >= 2)) {
+      dpr = 2;
+    } else {
+      dpr = 1;
+    }
+  } else {
+    dpr = 1;
+  }
+
+  const scale = 1 / dpr;
+  const docEl = document.documentElement;
+  let metaEl = document.querySelector("meta[name='viewport']");
+  const headEl = document.querySelector('head');
+
+  if (!metaEl) {
+    metaEl = document.createElement('meta');
+    metaEl.setAttribute('name', 'viewport');
+    if (headEl) {
+      headEl.appendChild(metaEl);
+    }
+  }
+  const fn = () => {
+    let docElWidth = docEl.getBoundingClientRect().width;
+    if (docElWidth / dpr > 540) {
+      docElWidth = 540 * dpr;
+    }
+    const fontSize = docElWidth / 10;
+    if (metaEl) {
+      metaEl.setAttribute(
+        'content',
+        'initial-scale=' +
+          scale +
+          ',maximum-scale=' +
+          scale +
+          ', minimum-scale=' +
+          scale +
+          ',user-scalable=no'
+      );
+    }
+    docEl.setAttribute('data-dpr', dpr.toString());
+    docEl.style.fontSize = fontSize + 'px';
+  };
+  window.addEventListener('resize', fn, false);
+  document.addEventListener('DOMContentLoaded', fn, false);
+}
