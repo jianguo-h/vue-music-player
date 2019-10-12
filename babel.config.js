c7ef4180341aed53640036fef6fbba174a39ff10
jianguo <1060684755@qq.com>
2019-10-12 09:41:44 +0800

    feat: 各配置文件修改

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/env",
+      {
+        modules: false
+      }
+    ]
+  ],
+  plugins: [
+    "@babel/plugin-syntax-dynamic-import",
+    [
+      "component",
+      {
+        libraryName: "mint-ui",
+        style: true
+      }
+    ]
+  ]
+};
