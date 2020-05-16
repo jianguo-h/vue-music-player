4eaa3fa20c79dcd7e011138e6d1ccadccd63a1ba
jianguo-h <1060684755@qq.com>
2020-05-16 20:59:23 +0800

    style: format config dir, express dir files

--- config/index.js
+++ config/index.js
@@ -2,12 +2,12 @@ module.exports = {
   dev: {
     port: 8080,
     env: 'development',
-    publicPath: '/'
+    publicPath: '/',
   },
   prod: {
     env: 'production',
     publicPath: '/',
-    port: 8088
+    port: 8088,
   },
   // node服务器接口代理
   proxyTable: {
@@ -15,22 +15,22 @@ module.exports = {
     '/songsearch': {
       target: 'http://songsearch.kugou.com/song_search_v2',
       pathRewrite: {
-        '^/songsearch': ''
-      }
+        '^/songsearch': '',
+      },
     },
     // 获取歌曲接口
     '/play': {
       target: 'http://www.kugou.com/yy/index.php',
       pathRewrite: {
-        '^/play': ''
-      }
+        '^/play': '',
+      },
     },
     // 搜索框关键词搜索接口
     '/searchtip': {
       target: 'http://searchtip.kugou.com/getSearchTip',
       pathRewrite: {
-        '^/searchtip': ''
-      }
-    }
-  }
+        '^/searchtip': '',
+      },
+    },
+  },
 };
--- express/router.js
+++ express/router.js
@@ -1,13 +1,13 @@
 const songData = require('../static/data/song.json');
 
 // 配置express的路由
-module.exports = function(app) {
+module.exports = function (app) {
   const routes = ['new', 'recommend', 'local'];
   for (const route of routes) {
     app.post('/api/' + route, (req, res) => {
       res.json({
         path: route,
-        data: songData[route]
+        data: songData[route],
       });
     });
   }
