42b4f8a57a5ff09b7c1cec5bd88f08df4eaf1215
jianguo-h <1060684755@qq.com>
2022-01-21 14:13:25 +0800

    feat: add stylelint-config-standard

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  extends: 'stylelint-prettier/recommended',
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
   rules: {
+    'selector-class-pattern': null,
     'comment-empty-line-before': 'always',
     'rule-empty-line-before': [
       'always',
       { ignore: ['after-comment', 'inside-block'] },
     ],
   },
+  overrides: [
+    {
+      files: ['**/*.less'],
+      customSyntax: 'postcss-less',
+    },
+    {
+      files: ['**/*.html', '**/*.vue'],
+      customSyntax: 'postcss-html',
+    },
+  ],
 };
